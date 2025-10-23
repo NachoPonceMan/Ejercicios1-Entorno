636c519a2402288dbbdc05341d049a3aba980712
NachoPonceMan~ <ignacioponceman@gmail.com~>
2025-10-23 11:07:52 +0200

    ej5.js

---
+++ ej5.js
@@ -0,0 +1,11 @@
+const prompt = require (`prompt-sync`);
+
+let num = prompt ('Introduce un numero');
+
+if (num = 0, num < 0) {
+console.log ('Error, el numero introducido es invalido.')
+ 
+} else { 
+
+
+}
