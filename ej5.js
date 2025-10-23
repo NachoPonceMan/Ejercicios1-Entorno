88b963ca7b09610219808244ba416154ef3c5a48
NachoPonceMan <ignacioponceman@gmail.com>
2025-10-23 13:31:31 +0200

    Update ej5.js

--- ej5.js
+++ ej5.js
@@ -1,11 +1,11 @@
-const prompt = require (`prompt-sync`);
+const prompt = require('prompt-sync')();
 
-let num = prompt ('Introduce un numero');
+let num = Number(prompt('Introduce un número: '));
 
-if (num = 0, num < 0) {
-console.log ('Error, el numero introducido es invalido.')
- 
-} else { 
-
-
+if (num <= 0) {
+    console.log('Error: el número introducido es inválido.');
+} else {
+    let cuadrado = num ** 2;
+    let raiz = Math.sqrt(num);
+    console.log(`Del número ${num}, su potencia es ${cuadrado} y su raíz es ${raiz}`);
 }
