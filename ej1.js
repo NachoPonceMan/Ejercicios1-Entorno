19fd059f75134be52f798a3e6034b273aee44fa4
NachoPonceMan~ <ignacioponceman@gmail.com~>
2025-10-23 11:06:47 +0200

    ej1.js

---
+++ ej1.js
@@ -0,0 +1,20 @@
+//Lee desde entrada dos variables numéricas A y B.
+//Con ellas se pide realizar un algoritmo que intercambie los valores de ambas variables y muestre cuánto valen al final las dos variables.
+
+    let prompt = require(`prompt-sync`)();
+
+    let a = prompt("Introduzca a:");
+    //typeof a == `string`
+    a = Number(a);
+    //typeof a == `number`
+    let b = prompt("Introduzca b:");
+    b = Number(b);
+
+    //Patrón de intercambio de variables
+    let aux = a;
+    a = b;
+    b = aux;
+    
+    //Printo mediante interpolación de cadena en JS con `$(variable)
+    console.log("El valor de a = $(a) y b = $(b)");
+
